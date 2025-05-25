7e2f4709ad8cb69bd53fdb9ea09d121848d08120
Boritz <81463588+Boritz@users.noreply.github.com>
2025-05-25 08:51:25 +0100

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,10 @@ from app import create_app
 
 app = create_app()
 
+@app.route("/")
+def index():
+    return "Lounge bot is running!"
+
 if __name__ == "__main__":
     logging.info("Flask app started")
     app.run()
